3266b038eae1047b1c3b58c5e054556093ccceee
dntmdwls42 <dntmdwls42@wku.ac.kr>
2024-08-03 12:52:38 +0900

    First Commit.. also first push on macOS

---
+++ src/java_01/Java_01_01.java
@@ -0,0 +1,8 @@
+package java_01;
+
+public class Java_01_01 {
+    public static void main(String[] args) {
+        System.out.println("macOS에서 해보는 첫 번째 프로그래밍");
+        System.out.println("여긴 정말 색다르군요. 낮설지만 익숙한 느낌입니다.");
+    }
+}
